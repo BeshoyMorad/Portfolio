48676f987e609e1b89baddccb482976d01fa0307
BeshoyMorad <besh0morta@gmail.com>
2021-09-02 00:47:14 +0200

    added the skills and contact sections

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,9 +1,11 @@
 //Headers Functions
 let menuIcon = document.querySelector(`header nav .icon`);
 let menuUL = document.querySelector(`header nav ul`);
+let menuSpans = document.querySelectorAll(`header nav .icon span`);
 
 document.addEventListener(`click`, (e) => {
-  if (e.target === menuIcon || (e.target.tagName === `SPAN` && e.target.id === ``)) {
+  if (e.target === menuIcon || e.target === menuSpans[0] || 
+    e.target === menuSpans[1] || e.target === menuSpans[2]) {
     menuUL.classList.toggle(`active`);
   } else if (e.target !== menuUL && e.target.tagName !== `LI`) {
     if (menuUL.classList.contains(`active`)) {
@@ -15,3 +17,4 @@ document.addEventListener(`click`, (e) => {
 let ageSpan = document.getElementById(`age`);
 let todayDate = new Date();
 ageSpan.innerText = todayDate.getFullYear() - 2002;
+
