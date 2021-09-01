d56f3ac9bd6e2d9b42733441b08340b3c4127965
BeshoyMorad <besh0morta@gmail.com>
2021-09-01 15:58:29 +0200

    edit the header icon and added dynamic age

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,10 +1,9 @@
 //Headers Functions
-let menuIcon = document.querySelector(`header nav .icon i`);
+let menuIcon = document.querySelector(`header nav .icon`);
 let menuUL = document.querySelector(`header nav ul`);
 
 document.addEventListener(`click`, (e) => {
-  console.log()
-  if (e.target === menuIcon) {
+  if (e.target === menuIcon || (e.target.tagName === `SPAN` && e.target.id === ``)) {
     menuUL.classList.toggle(`active`);
   } else if (e.target !== menuUL && e.target.tagName !== `LI`) {
     if (menuUL.classList.contains(`active`)) {
@@ -13,3 +12,6 @@ document.addEventListener(`click`, (e) => {
   }
 })
 
+let ageSpan = document.getElementById(`age`);
+let todayDate = new Date();
+ageSpan.innerText = todayDate.getFullYear() - 2002;
