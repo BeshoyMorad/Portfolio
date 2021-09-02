2ab0e5025a6261476d186ef890e681bbf4e83581
BeshoyMorad <besh0morta@gmail.com>
2021-09-02 16:55:13 +0200

    added the projects section and edited the contact section

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -18,3 +18,35 @@ let ageSpan = document.getElementById(`age`);
 let todayDate = new Date();
 ageSpan.innerText = todayDate.getFullYear() - 2002;
 
+
+//Projects Functions
+let nextButton = document.querySelector(`.projects .projects-container .next`);
+let prevButton = document.querySelector(`.projects .projects-container .prev`);
+let projectItems = document.querySelector(`.projects .projects-container .project-items`).children;
+let projectCount = projectItems.length;
+let index = 0;
+
+nextButton.onclick = function() {
+  if (index < projectCount - 1) {
+    index++;
+  }else {
+    index = 0;
+  }
+  slide();
+}
+
+prevButton.onclick = function() {
+  if (index > 0) {
+    index--;
+  }else {
+    index = projectCount - 1;
+  }
+  slide();
+}
+
+function slide() {
+  for (var i = 0; i < projectCount; i++) {
+    projectItems[i].classList.remove(`active`);
+  };
+  projectItems[index].classList.add(`active`);
+}
